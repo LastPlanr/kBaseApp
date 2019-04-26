32df831e428d10554369a0bf18de82e0d7db8474
cleber <cleber@dronemapp.com>
2019-04-26 15:23:29 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.1.0'
+
+with open('requirements/production.txt') as requirements_file:
+    requires = [item for item in requirements_file]
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='kbaseapp',
+    version=version,
+    description="Kotoko basic app structure",
+    long_description=readme,
+    author='Cléber Zavadniak',
+    author_email='cleberman@gmail.com',
+    url='https://github.com/Dronemapp/kbaseapp',
+    license=license,
+    packages=['kbaseapp'],
+    package_data={'': ['AUTHORS.md', 'README.md']},
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False,
+    keywords='generic libraries',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+)
